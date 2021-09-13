4861bdb3d6bc0d007740780a132d3bbfcac40d29
Quist0n <37319086+Quist0n@users.noreply.github.com>
2021-09-13 17:49:34 -0500

    fix(db): Removed IF EXISTS and IF NOT EXISTS checks from notification_type creation and deletion (doesn't work) and removed unneeded removal of indexes from rollback step. This works now.

--- migrations/20210913_01_nisj-add-notification-table.py
+++ migrations/20210913_01_nisj-add-notification-table.py
@@ -8,7 +8,7 @@ __depends__ = {"20210814_01_xwCee_add_role_column_to_accounts_table"}
 steps = [
     step(
     """
-    CREATE TYPE IF NOT EXISTS notification_types as ENUM ('none', 'artist_updated', 'account_promotion', 'account_demotion', 'administrative');
+    CREATE TYPE notification_types as ENUM ('none', 'artist_updated', 'account_promotion', 'account_demotion', 'administrative');
     CREATE TABLE IF NOT EXISTS notification (
         id BIGSERIAL PRIMARY KEY,
         account_id INT NOT NULL,
@@ -27,13 +27,8 @@ steps = [
     """,
 
     """
-    DROP TYPE notification_types;
     DROP TABLE IF EXISTS notification;
-    DROP INDEX IF EXISTS notification_account_id_idx;
-    DROP INDEX IF EXISTS notification_created_at_idx;
-    DROP INDEX IF EXISTS notification_type_idx;
-    DROP INDEX IF EXISTS notification_categories_idx;
-    DROP INDEX IF EXISTS notification_extra_info_idx;
+    DROP TYPE notification_types;
     """
     )
     ]
