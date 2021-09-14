7c72c3ab8638e8969f98d653b5d381afab4ea8bb
Qurtin <37319086+Quist0n@users.noreply.github.com>
2021-09-14 17:27:27 +0000

    Add DB migrations for future notification system (#5)
    * feat(db): Make table and indexes for notification system
    
    * feat(db): Modify and improve types for notifications table as well as rename some columns.
    
    * feat(db): Add IF NOT EXISTS to creation of notification_types ENUM
    
    * typo(db): Fix placement of IF NOT EXISTS for creation of notification_types ENUM.
    
    * fix(db): Removed IF EXISTS and IF NOT EXISTS checks from notification_type creation and deletion (doesn't work) and removed unneeded removal of indexes from rollback step. This works now.
    
    * Change notification_type to replace promotion and demotion types with 'account_role_changed' type
    
    * feat(db): Remove notification_type, change type of notification to INT instead. Allows for more flexible types of notifications in future.
    
    * fix(db): Remove DROP TYPE notification_types from migration. No longer needed
    
    * fix(db): Remove default value for type column.
    
    * feat(db): Change type column to type SMALLINT
    
    * feat(db): Remove categories column for now. Remove indexes for categories and extra_info columns

---
+++ migrations/20210913_01_nisj-add-notification-table.py
@@ -0,0 +1,29 @@
+
+"""
+Add notification table with indexes
+"""
+from yoyo import step
+__depends__ = {"20210814_01_xwCee_add_role_column_to_accounts_table"}
+
+steps = [
+    step(
+    """
+    CREATE TABLE IF NOT EXISTS notification (
+        id BIGSERIAL PRIMARY KEY,
+        account_id INT NOT NULL,
+        type SMALLINT NOT NULL,
+        extra_info jsonb,
+        created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (account_id) REFERENCES account(id)
+    );
+
+    CREATE INDEX IF NOT EXISTS notification_account_id_idx ON notification USING BTREE ("account_id");
+    CREATE INDEX IF NOT EXISTS notification_created_at_idx ON notification USING BTREE ("created_at");
+    CREATE INDEX IF NOT EXISTS notification_type_idx ON notification USING BTREE ("type");
+    """,
+
+    """
+    DROP TABLE IF EXISTS notification;
+    """
+    )
+    ]
