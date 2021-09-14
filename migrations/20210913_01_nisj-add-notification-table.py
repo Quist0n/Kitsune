7ceb878af22a8780c120e7c271eef15cf77b2bd1
Quist0n <37319086+Quist0n@users.noreply.github.com>
2021-09-14 11:06:10 -0500

    feat(db): Remove categories column for now. Remove indexes for categories and extra_info columns

--- migrations/20210913_01_nisj-add-notification-table.py
+++ migrations/20210913_01_nisj-add-notification-table.py
@@ -12,7 +12,6 @@ steps = [
         id BIGSERIAL PRIMARY KEY,
         account_id INT NOT NULL,
         type SMALLINT NOT NULL,
-        categories text[] NOT NULL,
         extra_info jsonb,
         created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
         FOREIGN KEY (account_id) REFERENCES account(id)
@@ -21,8 +20,6 @@ steps = [
     CREATE INDEX IF NOT EXISTS notification_account_id_idx ON notification USING BTREE ("account_id");
     CREATE INDEX IF NOT EXISTS notification_created_at_idx ON notification USING BTREE ("created_at");
     CREATE INDEX IF NOT EXISTS notification_type_idx ON notification USING BTREE ("type");
-    CREATE INDEX IF NOT EXISTS notification_categories_idx ON notification USING GIN ("categories");
-    CREATE INDEX IF NOT EXISTS notification_extra_info_idx ON notification USING GIN ("extra_info");
     """,
 
     """
