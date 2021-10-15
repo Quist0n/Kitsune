5768f2017ce8c8e09d7ae39527651efac937e89a
Quist0n <37319086+Quist0n@users.noreply.github.com>
2021-10-15 01:58:05 -0500

    dev(test-data-generators): Partly fill out functions for generating random test data

--- development/lib/importer/randoms.py
+++ development/lib/importer/randoms.py
@@ -3,37 +3,58 @@ from development.types import Extended_Random
 
 from .types import Post, User, DM, Comment, Embed, File, Attachment
 
-max_date = '' # ISO timestamp
-seed = 'seed'
+
+seed = 'Kitsune_Sneedy_Seed'
 test_random = Extended_Random(seed)
+
 
 def random_post() -> Post:
     post = Post(
-        id= test_random.string(0, 255),
-        user_id= test_random.string(0, 255),
-        service= service_name,
+        id = test_random.string(0, 255),
+        user_id = test_random.string(0, 255),
+        service = service_name
     )
     return post
 
 
 def random_user() -> User:
-    pass
+    user = User(
+            id = test_random.string(0, 255),
+        service = service_name
+
+    )
+    return user
 
 
 def random_dm() -> DM:
-    pass
+    dm = DM(
+            id = test_random.string(0, 255),
+            user_id = test_random.string(0, 255),
+            service = service_name,
+            content = test_random.string(0, 128),
+            embed = random_embed(),
+            file = random_file(),
+            published = test_random.date(),
+    )
+
+    return dm
 
 
 def random_comment() -> Comment:
     pass
 
+
 def random_embed() -> Embed:
-    embed = Embed()
+    embed = Embed(
+
+    )
     return embed
+
 
 def random_file() -> File:
     file = File()
     return file
+
 
 def random_attachment() -> Attachment:
     attachment = Attachment()
--- development/lib/importer/users.py
+++ development/lib/importer/users.py
@@ -7,12 +7,16 @@ from .types import User
 
 def import_users(import_id: str, key: str):
     """Imports test users."""
-    users: List[User] = []
+    user_amount = range(test_random.randint(23, 117))
+    users: List[User] = [ random_user() for user in user_amount ]
 
-    for user in users:
-        import_user(import_id, key, user)
+    if users
+        for user in users:
+            log(import_id, f"Importing user {user['id']}")
+            import_user(import_id, key, user)
 
-
+    else:
+        log(import_id, f"User not supplied. Will not be imported."))
 def import_user(import_id: str, key: str, user: User):
     """Imports a test user."""
 
@@ -22,3 +26,26 @@ def import_user(import_id: str, key: str, user: User):
 
     import_comments(import_id, key, user)
     log(import_id, f"Starting import: {user['id']} from user {user['id']}")
+    save_user_to_db(user)
+
+def save_user_to_db(user: User):
+    query_args = dict(
+    fields = ",".join(user.keys()),
+    values = ",".join(user.values()),
+    )
+
+    query = """
+        INSERT INTO lookup (%(fields)s)
+        VALUES (%(values)s)
+        ON CONFLICT (id, service)
+            DO NOTHING;
+    """
+
+    conn = get_raw_conn()
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute(query, query_args)
+        conn.commit()
+    finally:
+        return_conn(conn)
