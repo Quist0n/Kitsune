e6aad206023061568b4c5af1b3f8a09b3499c541
Quist0n <37319086+Quist0n@users.noreply.github.com>
2021-10-18 21:41:22 -0500

    feat(db): Allow non-empty test attachements

--- development/lib/importer/posts.py
+++ development/lib/importer/posts.py
@@ -40,15 +40,14 @@ def save_post_to_db(post: Post):
         user = post['user'],
         service = post['service'],
         file = json.dumps(post['file']),
-        #TODO: Get parsing of attachements for query right
-        attachments = json.dumps({}),
+        attachments = [json.dumps(post['attachments'])],
     )
 
     print("Query params:\n", query_params)
     print("Post info:\n", post)
     query = """
     INSERT INTO posts (id, \"user\", service, file, attachments)
-    VALUES (%(id)s, %(user)s, %(service)s, %(file)s, %(attachments)s)
+    VALUES (%(id)s, %(user)s, %(service)s, %(file)s, %(attachments)s::jsonb[])
     ON CONFLICT (id, service) DO NOTHING
     """
 
