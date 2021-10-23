6ba0d02044b521d25f1187b2c39208bec6bd122a
BassOfBass <vinniethepooloo@protonmail.com>
2021-10-23 15:59:24 +0500

    chore(dev): file saving (unfinished)

--- development/utils/files.py
+++ development/utils/files.py
@@ -1,4 +1,13 @@
+import os
+import tempfile
+import shutil
 from pathlib import Path
+from uuid import uuid4
+from datetime import datetime
+
+from configs.derived_vars import temp_dir_root, data_folder
+from src.internals.utils.utils import get_hash_of_file
+from development.types.models import File, File_Meta
 
 from typing import List
 
@@ -31,14 +40,41 @@ def get_folder_file_paths(folder: Path, extensions: List[str] = None) -> List[Pa
     return files
 
 
-def get_file_from_path(path: Path):
+def get_file_from_path(file_path: Path):
     """
     Gets the file out of provided absolute path.
     """
-    file = None
+    temp_path = create_temporary_directory()
+    hash_filename = generate_hashy_filename(temp_path)
+    file_name = temp_dir_root.joinpath(temp_path.name)
+    mtime = datetime.fromtimestamp(file_name.stat().st_mtime)
+    ctime = datetime.fromtimestamp(file_name.stat().st_mtime)
+    real_path = Path(data_folder, hash_filename)
+
+    if real_path.exists():
+        shutil.rmtree(temp_path.parent, ignore_errors=True)
+        return (Path(temp_path.name, hash_filename), file_path)
+
+    file = File(
+        mtime=mtime,
+        ctime=ctime
+    )
     return file
 
 
-def uniquify_filename(path: Path):
-    new_filename = None
-    return new_filename
+def create_temporary_directory() -> Path:
+    """"""
+    os.makedirs(temp_dir_root, exist_ok=True)
+    temp_dir = tempfile.mkdtemp(dir=temp_dir_root)
+    temp_name = f'{str(uuid4())}.temp'
+    temp_path = Path(temp_dir, temp_name)
+    return temp_path
+
+
+def generate_hashy_filename(file_path: Path):
+    """
+    Generates unique filename.
+    """
+    file_hash = get_hash_of_file(file_path)
+    hash_filename = Path(file_hash[0:2], file_hash[2:4], file_hash + file_path.suffix)
+    return hash_filename
